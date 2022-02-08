6ea3047657e5a5b4c1b088eab7a68e5273dba605
nc-minh <nguyencongminh.official@gmail.com>
2022-02-08 14:59:33 +0700

    :revolving_hearts: Code fall in love

--- react-hooks/src/components/useEffect.jsx
+++ react-hooks/src/components/useEffect.jsx
@@ -26,31 +26,81 @@ import { useState, useEffect } from 'react'
 // }
 
 // 2. useEffect(callback, [])
+// function Content() {
+//   const [title, setTitle] = useState('')
+//   const [posts, setPosts] = useState([])
+
+//   useEffect(() => {
+//     console.log('Mounted!')
+//     document.title = title
+//     fetch('https://jsonplaceholder.typicode.com/users')
+//       .then(res => res.json())
+//       .then(posts => {
+//         setPosts(posts)
+//       })
+//   }, [])
+  
+//   console.log(title)
+//   console.log(posts)
+//   return (
+//     <div>
+//       <input
+//         value={title}
+//         onChange={e => setTitle(e.target.value)}
+//       />
+//       <ul>
+//         {posts.map(post => (
+//           <li key={post.id}>{post.name}</li>
+//         ))}
+//       </ul>
+//     </div>
+    
+//   );
+// }
+
+// 3. useEffect(callback, [deps])
+const tabs = ['posts', 'comments', 'albums']
 function Content() {
   const [title, setTitle] = useState('')
   const [posts, setPosts] = useState([])
+  const [type, setType] = useState('posts')
+  console.log(type)
 
   useEffect(() => {
     console.log('Mounted!')
     document.title = title
-    fetch('https://jsonplaceholder.typicode.com/users')
+    fetch(`https://jsonplaceholder.typicode.com/${type}`)
       .then(res => res.json())
       .then(posts => {
         setPosts(posts)
       })
-  }, [])
+  }, [type])
   
   console.log(title)
   console.log(posts)
   return (
     <div>
+      {
+        tabs.map(tab => (
+          <button 
+            key={tab}
+            style={type === tab ? {
+              color: '#fff',
+              backgroundColor: '#333'
+            } : {}}
+            onClick={() => setType(tab)}
+          >
+            {tab}
+          </button>
+        ))
+      }
       <input
         value={title}
         onChange={e => setTitle(e.target.value)}
       />
       <ul>
         {posts.map(post => (
-          <li key={post.id}>{post.name}</li>
+          <li key={post.id}>{post.title || post.name}</li>
         ))}
       </ul>
     </div>
